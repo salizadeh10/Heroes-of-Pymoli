62f8cf7d5cc1c2def8f577b83e722fbadf8b6e53
salizadeh10 <38050160+salizadeh10@users.noreply.github.com>
2018-08-24 19:46:46 -0700

    Completed through - Purchasing Analysis (Gender)

--- Heroes-of-Pymoli.py
+++ Heroes-of-Pymoli.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[101]:
+# In[32]:
 
 
 # Dependencies and Setup
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 
-# In[102]:
+# In[33]:
 
 
 # Load the file
@@ -19,20 +19,20 @@ dataFile = "purchase_data.csv"
 purchase_data_pd = pd.read_csv(dataFile)
 
 
-# In[103]:
+# In[34]:
 
 
 purchase_data_pd.head()
 
 
-# In[104]:
+# In[35]:
 
 
 player_count = len(purchase_data_pd["SN"].value_counts())
 print("Total Number of Players: " + str(player_count))
 
 
-# In[105]:
+# In[36]:
 
 
 # Purchasing Analysis (Total)
@@ -42,7 +42,7 @@ print("Total Number of Players: " + str(player_count))
 # Display the summary data frame
 
 
-# In[106]:
+# In[37]:
 
 
 # get number of unique purchases
@@ -50,7 +50,7 @@ unique_items = len(purchase_data_pd["Item ID"].value_counts())
 print("Number of unique purchase items: " + str(unique_items))
 
 
-# In[107]:
+# In[38]:
 
 
 # get the average price of items
@@ -58,7 +58,7 @@ avg_price = round(purchase_data_pd["Price"].mean(), 2)
 print("Average price of items is: " + str(avg_price))
 
 
-# In[108]:
+# In[39]:
 
 
 # get number of purchased 
@@ -66,7 +66,7 @@ number_of_purchases = len(purchase_data_pd["Purchase ID"].value_counts())
 print("Number of items purchased: " + str(number_of_purchases))
 
 
-# In[109]:
+# In[40]:
 
 
 # total revenue generated from sales
@@ -74,7 +74,7 @@ total_revenue = purchase_data_pd["Price"].sum()
 print("Total revenue is: " + str(total_revenue))
 
 
-# In[110]:
+# In[41]:
 
 
 # Create dataframe for number of items, avg sale price, number of purchases, and total revenue
@@ -90,7 +90,7 @@ purchase_df = pd.DataFrame([purchase_summary], columns = ["Number of Unique Item
 purchase_df.head()
 
 
-# In[111]:
+# In[42]:
 
 
 # Gender Demographics
@@ -130,8 +130,55 @@ demographics_df = demographics_df.set_index("Gender")
 demographics_df.style.format({"Percentage of Players": "{:.2f}%"})  
 
 
+# In[47]:
+
+
+# Purchasing Analysis (Gender)
+# Run basic calculations to obtain purchase count, avg. purchase price, avg. purchase total per person etc. by gender
+# Create a summary data frame to hold the results
+# Optional: give the displayed data cleaner formatting
+# Display the summary data frame
+
+
+# In[87]:
+
+
+# Purchase counts by Gender column
+
+# Purchases by male players
+is_male = purchase_data_pd["Gender"] == "Male"
+males_purchases_df = purchase_data_pd[is_male]
+males_purchase_count = males_purchases_df.shape[0]
+males_avg_purchase_price = males_purchases_df
+males_total_spend = round((males_purchases_df["Price"].sum()), 2)
+males_avg_spend = round((males_total_spend / males_purchase_count), 2)
+
+# Purchases by female players
+is_female = purchase_data_pd["Gender"] == "Female"
+females_purchases_df = purchase_data_pd[is_female]
+females_purchase_count = females_purchases_df.shape[0]
+females_total_spend = round((females_purchases_df["Price"].sum()), 2)
+females_avg_spend = round((females_total_spend / females_purchase_count), 2)
+
+# Purchases - Other.Non dislosed
+other_non_disclosed_purchase_count = number_of_purchases - (males_purchase_count + females_purchase_count)
+other_non_disclosed_total_spend = round((total_revenue - (males_total_spend + females_total_spend)), 2)
+other_non_disclosed_avg_spend = round((other_non_disclosed_total_spend / other_non_disclosed_purchase_count), 2)
+
+# Create the dataframe for purchasing data analysis
+purchase_analysis_df = pd.DataFrame({"Gender": ["Male", "Female", "Other / Non-Disclosed"], 
+                                    "Purchase Count": [males_purchase_count, females_purchase_count, 
+                                                              other_non_disclosed_purchase_count],
+                                    "Average Purchase Price": [males_avg_spend, females_avg_spend, other_non_disclosed_avg_spend],
+                                    "Total Purchase Value": [males_total_spend, females_total_spend, other_non_disclosed_total_spend]}, 
+                               columns = ["Gender", "Purchase Count", "Average Purchase Price", "Total Purchase Value"])
+
+purchase_analysis_df = purchase_analysis_df.set_index("Gender")
+purchase_analysis_df
+
+
 # In[ ]:
 
 
-get_ipython().system('jupyter nbconvert --to script winning_wrestlers.ipynb')
+get_ipython().system('jupyter nbconvert --to script Heroes-of-Pymoli.ipynb')
 
